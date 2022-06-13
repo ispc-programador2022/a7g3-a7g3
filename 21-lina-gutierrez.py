5d0154734baa853c889ddafa411a3a810ffb8da0
DMI <dmi@DMIs-MacBook-Pro.local>
2022-06-13 13:19:31 -0300

    Fixed test

--- 21-lina-gutierrez.py
+++ 21-lina-gutierrez.py
@@ -1,5 +1,3 @@
-import random
-
 # LA NECESITO PARA PODER CALCULAR LA FUNCION 21 => 06 - función que calcule el rango del vector obtenido en genrnd.
 def rangeVector (vector): 
     vector = sorted(vector)
@@ -9,16 +7,3 @@ def rangeVector (vector):
 #  21 - función que calcule devuelva el maximo del vector obtenido en genrnd.
 def maxVector (vector): 
     return rangeVector(vector)[1]
-
-#### TESTING ####
-
-# LA NECESITO PARA PODER TESTEAR LA FUNCION 21 => función genrnd que retorna una lista con 50 números aleatorios.
-def genrnd(): 
-    listArray = []
-    for i in range(50):
-        listArray.append(random.randint(-500, 500))
-    return listArray
-
-vector = genrnd()
-
-print('Resultado de la actividad 21 =>', minVector (vector))

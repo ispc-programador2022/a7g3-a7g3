4cb5ea2daf9f4ab287759e1ebdb9afcebfd830a4
lilenguzman01 <lilenguzman2015@gmail.com>
2022-06-28 11:37:02 -0300

    Issue#02_Lilen Guzman

---
+++ SP1/ing2s.py
@@ -0,0 +1,6 @@
+#función ing2s, debe permitir el ingreso de 2 valores string
+def ing2s():
+    cadena1=input("Ingrese primer string")
+    cadena2=input("Ingrese segundo string")
+    return(cadena1,cadena2)
+

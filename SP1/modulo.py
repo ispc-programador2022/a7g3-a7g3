59aa34410d59fc041fa2be0f98c37ece1a27ceb3
NIRIANLYSGUZMAN <Nirianlysguzman@gmail.com>
2022-07-02 10:26:42 -0300

    Issue#07-Niri Guzmán

---
+++ SP1/modulo.py
@@ -0,0 +1,8 @@
+# función módulo, retorna el módulo de 2 parámetros:
+
+n1=int(input("Ingrese dividendo"))
+n2=int(input("Ingrese divisor"))
+
+def modulos (n1,n2): return n1%n2 
+
+print("El residuo es ", modulos(n1,n2))
--- SP1/modulo1.py
+++
@@ -1,8 +0,0 @@
-# función módulo, retorna el módulo de 2 parámetros:
-
-N1=int(input("Ingrese dividendo"))
-N2=int(input("Ingrese divisor"))
-
-def modulos (N1,N2): return N1%N2 
-
-print("El residuo es ", modulos(N1,N2))

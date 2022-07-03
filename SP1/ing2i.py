5465074f864b7db82577fc9c116d9c3db637b411
Euge Inga <eugeinga15@gmail.com>
2022-07-02 22:42:10 -0300

    01 Función 'ing2i' #3

--- SP1/ing2i.py
+++ SP1/ing2i.py
@@ -1,5 +1,4 @@
 #función ing2i, debe permitir el ingreso de 2 valores enteros
-
 def ing2i():
     int1 = int(input("Ingrese el primer valor entero: "))
     int2 = int(input("Ingrese el segundo valor entero: "))

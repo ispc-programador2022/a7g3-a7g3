73badda2041b7ae3ab82161cbf9f4ff9cc9affab
lilenguzman01 <lilenguzman2015@gmail.com>
2022-07-10 01:52:00 -0300

    actualice con lo hecho en el zoom del viernes 07/07

--- SP1/producto.py
+++ SP1/producto.py
@@ -1,15 +1,3 @@
-# función producto, retorna la multiplicacion de 2 parámetros.
-n1=int(input("ingrese un numero: "))
-n2=int(input("ingrese un numero: "))
 def multiplicacion (n1, n2): 
     return n1 * n2
 
-print("El producto es:", multiplicacion(n1, n2))
-
-#Testing
-n1=3
-n2=5
-def multiplicacion (n1, n2): 
-    return n1 * n2
-
-print("El producto es:", multiplicacion(n1, n2))

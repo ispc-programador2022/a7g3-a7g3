e769ce0ba4ac4342786567ea172620ee2c44b1fb
Caro <caritoinf@gmail.com>
2022-07-27 06:47:09 -0300

    05 Función 'producto'

--- SP1/potencia.py
+++ SP1/potencia.py
@@ -1,6 +1,4 @@
 # función potencia, retorna la  retorna la potencia del primero elevado al segundo parámetros
-n1=int(input("ingrese un numero: "))
-n2=int(input("ingrese un numero: "))
 def potencia (n1, n2): 
     return n1 ** n2
 
